932858ffb3ca1ea33a7f342b2321f3c894692c54
imkaiming <75501647+imkaiming@users.noreply.github.com>
2026-03-10 21:01:29 +0100

    incorrect tracked modules

--- modules/clap-juce-extensions/clap-libs/clap-helpers/cmake/clap-helpers-config.cmake
+++
@@ -1,4 +0,0 @@
-if (NOT TARGET clap-helpers)
-    add_library(clap-helpers INTERFACE IMPORTED GLOBAL)
-    target_include_directories(clap-helpers SYSTEM INTERFACE "${CMAKE_CURRENT_LIST_DIR}/../../../include")
-endif()
